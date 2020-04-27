13480ae2f213f1439e2bc9aa32aa941a87d7b209
bbaktaeho <lth9036@gmail.com>
2020-04-27 12:08:31 +0900

    [init] views 셋팅

---
+++ src/views/homeView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default class StartView extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>홈(밥주는) 화면</Text>
+      </View>
+    );
+  }
+}
---
+++ src/views/statisticsView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default class StartView extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>통계 화면</Text>
+      </View>
+    );
+  }
+}
