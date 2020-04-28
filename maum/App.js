ebbbc475367b0ab842568412533cf443d15cc138
bbaktaeho <lth9036@gmail.com>
2020-04-28 13:21:58 +0900

    [add] intro

--- maum/App.js
+++ maum/App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {useEffect} from 'react';
 import {
   SafeAreaView,
@@ -24,6 +16,7 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import SplashScreen from 'react-native-splash-screen';
+import Intro from './src/views/intro';
 
 const App: () => React$Node = () => {
   useEffect(() => {
@@ -31,90 +24,7 @@ const App: () => React$Node = () => {
       SplashScreen.hide();
     }, 2000);
   }, []);
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+  return <Intro />;
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
---
+++ maum/src/views/intro/index.js
@@ -0,0 +1,150 @@
+import React, {Component} from 'react';
+import {
+  AppRegistry,
+  Alert,
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
+import Swiper from 'react-native-swiper';
+
+var styles = {
+  wrapper: {},
+  slide: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#8D8A8A',
+  },
+  text: {
+    flex: 1,
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  iosSkip: {
+    position: 'absolute',
+    right: 20,
+    top: 55,
+  },
+  andSkip: {
+    position: 'absolute',
+    right: 20,
+    top: 20,
+  },
+  skipText: {
+    color: '#000000',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  imageView: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+  },
+};
+
+export default () => (
+  <Swiper style={styles.wrapper} showsButtons loop={false}>
+    <View testID="Hello" style={styles.slide}>
+      <View style={styles.imageView}>
+        {Platform.OS === 'ios' && (
+          <TouchableOpacity style={styles.iosSkip}>
+            <Text style={styles.skipText}>SKIP</Text>
+          </TouchableOpacity>
+        )}
+        {Platform.OS === 'android' && (
+          <TouchableOpacity style={styles.andSkip}>
+            <Text style={styles.skipText}>SKIP</Text>
+          </TouchableOpacity>
+        )}
+
+        <Image
+          style={{width: 300, height: 200}}
+          source={require('../../../assets/images/intro1.png')}
+        />
+      </View>
+      <Text style={styles.text}>밥을 주면 알려줘요!</Text>
+    </View>
+    <View testID="Beautiful" style={styles.slide}>
+      <View style={styles.imageView}>
+        {Platform.OS === 'ios' && (
+          <TouchableOpacity style={styles.iosSkip}>
+            <Text style={styles.skipText}>SKIP</Text>
+          </TouchableOpacity>
+        )}
+        {Platform.OS === 'android' && (
+          <TouchableOpacity style={styles.andSkip}>
+            <Text style={styles.skipText}>SKIP</Text>
+          </TouchableOpacity>
+        )}
+        <Image
+          style={{width: 200, height: 200}}
+          source={require('../../../assets/images/intro2.png')}
+        />
+      </View>
+      <Text style={styles.text}>순위를 알 수 있어요!</Text>
+    </View>
+    <View testID="Simple" style={styles.slide}>
+      <View style={styles.imageView}>
+        {Platform.OS === 'ios' && (
+          <TouchableOpacity style={styles.iosSkip}>
+            <Text style={styles.skipText}>SKIP</Text>
+          </TouchableOpacity>
+        )}
+        {Platform.OS === 'android' && (
+          <TouchableOpacity style={styles.andSkip}>
+            <Text style={styles.skipText}>SKIP</Text>
+          </TouchableOpacity>
+        )}
+        <Image
+          style={{width: 200, height: 200}}
+          source={require('../../../assets/images/intro3.png')}
+        />
+      </View>
+      <Text style={styles.text}>통계를 확인해요!</Text>
+    </View>
+    <View testID="Simple" style={styles.slide}>
+      <View style={styles.imageView}>
+        {Platform.OS === 'ios' && (
+          <TouchableOpacity style={styles.iosSkip}>
+            <Text style={styles.skipText}>SKIP</Text>
+          </TouchableOpacity>
+        )}
+        {Platform.OS === 'android' && (
+          <TouchableOpacity style={styles.andSkip}>
+            <Text style={styles.skipText}>SKIP</Text>
+          </TouchableOpacity>
+        )}
+        <Image
+          style={{width: 250, height: 220}}
+          source={require('../../../assets/images/intro4.png')}
+        />
+      </View>
+      <Text style={styles.text}>안전한 가족 채팅!</Text>
+    </View>
+    <View testID="Simple" style={styles.slide}>
+      <View style={styles.imageView}>
+        {Platform.OS === 'ios' && (
+          <TouchableOpacity style={styles.iosSkip}>
+            <Text style={styles.skipText}>시작하기</Text>
+          </TouchableOpacity>
+        )}
+        {Platform.OS === 'android' && (
+          <TouchableOpacity style={styles.andSkip}>
+            <Text style={styles.skipText}>시작하기</Text>
+          </TouchableOpacity>
+        )}
+        <Image
+          style={{width: 200, height: 200}}
+          source={require('../../../assets/images/intro5.png')}
+        />
+      </View>
+      <Text style={styles.text}>마음이 갤러리!</Text>
+    </View>
+  </Swiper>
+);
