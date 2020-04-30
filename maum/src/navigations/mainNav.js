3a49ccb34186cfc4f91f69d809853c7cd6bb4a8d
bbaktaeho <lth9036@gmail.com>
2020-04-30 23:48:14 +0900

    [init] MainNav

--- maum/src/navigations/mainNav.js
+++ maum/src/navigations/mainNav.js
@@ -6,16 +6,38 @@ import Chat from '../views/chat';
 import Ranking from '../views/ranking';
 import Statistics from '../views/statistics';
 import Gallary from '../views/gallary';
+import Setting from '../views/setting';
 
 const Draw = createDrawerNavigator();
 export default () => {
   return (
     <Draw.Navigator initialRouteName="Home">
-      <Draw.Screen name="Home" component={Home} />
-      <Draw.Screen name="Chat" component={Chat} />
-      <Draw.Screen name="Ranking" component={Ranking} />
-      <Draw.Screen name="Statistics" component={Statistics} />
-      <Draw.Screen name="Gallary" component={Gallary} />
+      <Draw.Screen name="Home" component={Home} options={{drawerLabel: '홈'}} />
+      <Draw.Screen
+        name="Chat"
+        component={Chat}
+        options={{drawerLabel: '채팅'}}
+      />
+      <Draw.Screen
+        name="Ranking"
+        component={Ranking}
+        options={{drawerLabel: '랭킹'}}
+      />
+      <Draw.Screen
+        name="Statistics"
+        component={Statistics}
+        options={{drawerLabel: '통계'}}
+      />
+      <Draw.Screen
+        name="Gallary"
+        component={Gallary}
+        options={{drawerLabel: '앨범'}}
+      />
+      <Draw.Screen
+        name="Setting"
+        component={Gallary}
+        options={{drawerLabel: '설정'}}
+      />
     </Draw.Navigator>
   );
 };
---
+++ maum/src/views/setting/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+
+const Setting = props => {
+  return (
+    <SafeAreaView>
+      <Text>설정</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Setting;
